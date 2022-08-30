8b3b715d91b5cc01ceeea5d81179563941ea7455
mekolac <eminkolac@gmail.com>
2022-08-30 04:35:33 +0000

    Code Synced by DhiWise

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/**/**/*.{js,ts,jsx,tsx,html,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,html,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    screens: { lg: "1120px", xl: "1281px", "2xl": "1441px", "3xl": "1729px" },
+    extend: { colors: {} },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
